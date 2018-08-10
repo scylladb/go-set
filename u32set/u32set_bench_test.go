4c723040fce84654a60fcc7028e702fc07150c21
Michał Matczuk <michal@scylladb.com>
2018-08-10 12:57:34 +0200

    added gen_set.sh script to generate set types (#9)

---
+++ u32set/u32set_bench_test.go
@@ -0,0 +1,140 @@
+// Copyright (C) 2017 ScyllaDB
+// Use of this source code is governed by a ALv2-style
+// license that can be found at https://github.com/scylladb/go-set/LICENSE.
+
+package u32set
+
+import (
+	"github.com/fatih/set"
+	"testing"
+)
+
+func BenchmarkTypeSafeSetHasNonExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint32
+	e := createRandomObject(e1)
+	if v, ok := e.(uint32); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := New()
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkInterfaceSetHasNonExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint32
+	e := createRandomObject(e1)
+	if v, ok := e.(uint32); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := set.New(set.NonThreadSafe)
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkTypeSafeSetHasExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint32
+	e := createRandomObject(e1)
+	if v, ok := e.(uint32); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := New()
+	s.Add(e1)
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkInterfaceSetHasExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint32
+	e := createRandomObject(e1)
+	if v, ok := e.(uint32); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := set.New(set.NonThreadSafe)
+	s.Add(e1)
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkTypeSafeSetHasExistingMany(b *testing.B) {
+	s := New()
+	b.StopTimer()
+	var e1 uint32
+	for i := 0; i < 10000; i++ {
+		e := createRandomObject(e1)
+		if v, ok := e.(uint32); ok {
+			s.Add(v)
+			if i == 5000 {
+				e1 = v
+			}
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkInterfaceSetHasExistingMany(b *testing.B) {
+	s := set.New(set.NonThreadSafe)
+	b.StopTimer()
+	var e1 uint32
+	for i := 0; i < 10000; i++ {
+		e := createRandomObject(e1)
+		if v, ok := e.(uint32); ok {
+			s.Add(v)
+			if i == 5000 {
+				e1 = v
+			}
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkTypeSafeSetAdd(b *testing.B) {
+	b.StopTimer()
+	var e uint32
+	s := New()
+	objs := make([]uint32, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		e := createRandomObject(e)
+		if v, ok := e.(uint32); ok {
+			objs = append(objs, v)
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Add(objs[i])
+	}
+}
+
+func BenchmarkInterfaceSetAdd(b *testing.B) {
+	b.StopTimer()
+	var e uint32
+	s := set.New(set.NonThreadSafe)
+	objs := make([]uint32, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		e := createRandomObject(e)
+		if v, ok := e.(uint32); ok {
+			objs = append(objs, v)
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Add(objs[i])
+	}
+}
---
+++ u64set/u64set_bench_test.go
@@ -0,0 +1,140 @@
+// Copyright (C) 2017 ScyllaDB
+// Use of this source code is governed by a ALv2-style
+// license that can be found at https://github.com/scylladb/go-set/LICENSE.
+
+package u64set
+
+import (
+	"github.com/fatih/set"
+	"testing"
+)
+
+func BenchmarkTypeSafeSetHasNonExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint64
+	e := createRandomObject(e1)
+	if v, ok := e.(uint64); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := New()
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkInterfaceSetHasNonExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint64
+	e := createRandomObject(e1)
+	if v, ok := e.(uint64); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := set.New(set.NonThreadSafe)
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkTypeSafeSetHasExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint64
+	e := createRandomObject(e1)
+	if v, ok := e.(uint64); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := New()
+	s.Add(e1)
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkInterfaceSetHasExisting(b *testing.B) {
+	b.StopTimer()
+	var e1 uint64
+	e := createRandomObject(e1)
+	if v, ok := e.(uint64); ok {
+		e1 = v
+	}
+	b.StartTimer()
+	s := set.New(set.NonThreadSafe)
+	s.Add(e1)
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkTypeSafeSetHasExistingMany(b *testing.B) {
+	s := New()
+	b.StopTimer()
+	var e1 uint64
+	for i := 0; i < 10000; i++ {
+		e := createRandomObject(e1)
+		if v, ok := e.(uint64); ok {
+			s.Add(v)
+			if i == 5000 {
+				e1 = v
+			}
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkInterfaceSetHasExistingMany(b *testing.B) {
+	s := set.New(set.NonThreadSafe)
+	b.StopTimer()
+	var e1 uint64
+	for i := 0; i < 10000; i++ {
+		e := createRandomObject(e1)
+		if v, ok := e.(uint64); ok {
+			s.Add(v)
+			if i == 5000 {
+				e1 = v
+			}
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Has(e1)
+	}
+}
+
+func BenchmarkTypeSafeSetAdd(b *testing.B) {
+	b.StopTimer()
+	var e uint64
+	s := New()
+	objs := make([]uint64, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		e := createRandomObject(e)
+		if v, ok := e.(uint64); ok {
+			objs = append(objs, v)
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Add(objs[i])
+	}
+}
+
+func BenchmarkInterfaceSetAdd(b *testing.B) {
+	b.StopTimer()
+	var e uint64
+	s := set.New(set.NonThreadSafe)
+	objs := make([]uint64, 0, b.N)
+	for i := 0; i < b.N; i++ {
+		e := createRandomObject(e)
+		if v, ok := e.(uint64); ok {
+			objs = append(objs, v)
+		}
+	}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		s.Add(objs[i])
+	}
+}
